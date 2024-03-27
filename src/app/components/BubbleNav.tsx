7d895bc5dc56f2f1067db6c256e425146110ef48
DBryja <xileran@gmail.com>
2024-03-27 22:05:04 +0100

    Launch Storybook on Vercel

--- src/app/components/BubbleNav.tsx
+++ src/app/components/BubbleNav.tsx
@@ -7,7 +7,7 @@ export default function BubbleNav(){
         const bubble = document.querySelector(".bubble-nav__bubble") as HTMLDivElement;
 
         navItems.forEach((item) => {
-            item.addEventListener("mouseover", function(this:HTMLElement) {
+            item.addEventListener("mouseover", function (this: HTMLElement) {
                 const itemWidth = this.offsetWidth;
                 const itemLeft = this.offsetLeft;
 
@@ -17,24 +17,36 @@ export default function BubbleNav(){
                 bubble.style.top = "0"
             });
 
-            item.addEventListener("mouseleave", function(this:HTMLElement) {
-                const itemLeft = this.offsetLeft;
+            //DODAC TRANSFORMA NA MOUSEENTER NA OSIACH X I Y BEZ ANIMACJI
 
-                // bubble.style.transform = "0";
-                bubble.style.top = "50%"
+            item.addEventListener("mouseleave", function (this: HTMLElement, e: MouseEvent) {
+                const rect = this.getBoundingClientRect();
+                const rectCenterX = rect.left + rect.width / 2;
+                const rectCenterY = rect.top + rect.height / 2;
+                const multipliers = {top: 1, left: 1};
+                if (e.clientX < rectCenterX) multipliers.left = -1;
+                if (e.clientY < rectCenterY) multipliers.top = -1;
+                const offsetDeltaX = (e.clientX - rect.left) * multipliers.left;
+                const offsetDeltaY = rect.height* multipliers.top;
+
                 bubble.style.width = "0";
                 bubble.style.opacity = "0";
-                bubble.style.left = itemLeft - 10 + "px";
+                bubble.style.top = this.offsetTop + offsetDeltaY + "px"
+                bubble.style.left = this.offsetLeft + offsetDeltaX + "px";
+
+                setTimeout(() => {
+                    bubble.style.top = "0";
+                }, 200)
             });
         });
     }, []);
 
     return (
         <nav className={"bubble-nav"}>
-            <li className="bubble-nav__item">Item1</li>
-            <li className="bubble-nav__item">LongItem2</li>
-            <li className="bubble-nav__item">Item3</li>
-            <li className="bubble-nav__item">Item4</li>
+            <li className="bubble-nav__item" data-pos={"left"}>Item1</li>
+            <li className="bubble-nav__item active" data-pos={"left"}>LongItem2</li>
+            <li className="bubble-nav__item" data-pos={"right"}>Item3</li>
+            <li className="bubble-nav__item" data-pos={"right"}>Super Long Item3</li>
 
             <div className={"bubble-nav__bubble"}/>
         </nav>
