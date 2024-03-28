1febb5d8117de119ec720ee3c054918795737c3e
DBryja <xileran@gmail.com>
2024-03-28 20:57:24 +0100

    Update BubbleNav, add button ripple effect

--- src/app/components/BubbleNav.tsx
+++ src/app/components/BubbleNav.tsx
@@ -1,24 +1,46 @@
 import "../styles/_bubbleNav.scss";
 import React, {useEffect} from "react";
+import Link from "next/link";
+const returnSmaller = (a: number, b: number) => a < b ? a : b;
 
-export default function BubbleNav(){
+const createRipple = (e: MouseEvent) => {
+    const button = e.currentTarget as HTMLElement;
+    const rect = button.getBoundingClientRect();
+
+    const circle = document.createElement("span");
+    const diameter = Math.max(rect.width, rect.height);
+    const radius = diameter / 2;
+
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${e.clientX - (rect.left + radius)}px`;
+    circle.style.top = `${e.clientY - (rect.top + radius)}px`;
+    circle.classList.add("ripple");
+
+    const ripple = button.getElementsByClassName("ripple")[0];
+    ripple?.remove();
+
+    button.appendChild(circle);
+}
+
+export default function BubbleNav({menuItems, active} : {menuItems: {name: string, href?: string}[], active: number}){
     useEffect(() => {
         const navItems = document.querySelectorAll(".bubble-nav__item") as NodeListOf<HTMLElement>;
         const bubble = document.querySelector(".bubble-nav__bubble") as HTMLDivElement;
+        let timeoutId: NodeJS.Timeout | undefined;
 
         navItems.forEach((item) => {
             item.addEventListener("mouseover", function (this: HTMLElement) {
+                clearTimeout(timeoutId);
                 const itemWidth = this.offsetWidth;
                 const itemLeft = this.offsetLeft;
+                const itemHeight = this.offsetHeight;
 
                 bubble.style.opacity = "100%";
-                bubble.style.width = itemWidth + "px";
+                bubble.style.width = itemWidth + "px"
+                bubble.style.height= itemHeight + "px";
                 bubble.style.left = itemLeft + "px";
                 bubble.style.top = "0"
             });
-
-            //DODAC TRANSFORMA NA MOUSEENTER NA OSIACH X I Y BEZ ANIMACJI
-
             item.addEventListener("mouseleave", function (this: HTMLElement, e: MouseEvent) {
                 const rect = this.getBoundingClientRect();
                 const rectCenterX = rect.left + rect.width / 2;
@@ -26,28 +48,34 @@ export default function BubbleNav(){
                 const multipliers = {top: 1, left: 1};
                 if (e.clientX < rectCenterX) multipliers.left = -1;
                 if (e.clientY < rectCenterY) multipliers.top = -1;
-                const offsetDeltaX = (e.clientX - rect.left) * multipliers.left;
-                const offsetDeltaY = rect.height* multipliers.top;
+                const offsetDeltaX = returnSmaller((e.clientX - rect.left) * multipliers.left, 50);
+                const offsetDeltaY = returnSmaller(e.clientY - rect.height/2 * multipliers.top, 30*multipliers.top);
 
-                bubble.style.width = "0";
+                bubble.style.width = "10px";
+                bubble.style.height = "10px";
                 bubble.style.opacity = "0";
                 bubble.style.top = this.offsetTop + offsetDeltaY + "px"
                 bubble.style.left = this.offsetLeft + offsetDeltaX + "px";
 
-                setTimeout(() => {
-                    bubble.style.top = "0";
-                }, 200)
+                clearTimeout(timeoutId);
+                timeoutId = setTimeout(() => {
+                    bubble.style.top = "50%";
+                    bubble.style.left = "50%"
+                }, 300)
             });
+            item.addEventListener("click", createRipple);
         });
     }, []);
 
     return (
         <nav className={"bubble-nav"}>
-            <li className="bubble-nav__item" data-pos={"left"}>Item1</li>
-            <li className="bubble-nav__item active" data-pos={"left"}>LongItem2</li>
-            <li className="bubble-nav__item" data-pos={"right"}>Item3</li>
-            <li className="bubble-nav__item" data-pos={"right"}>Super Long Item3</li>
-
+            {menuItems.map((item, index) =>
+                    (<Link
+                        key={index}
+                        href={item.href || item.name}
+                        className={`bubble-nav__item ${index === active ? "active" : "'"}`}
+                    >{item.name}</Link>)
+                )}
             <div className={"bubble-nav__bubble"}/>
         </nav>
     )
--- src/stories/BubbleNav.stories.ts
+++ src/stories/BubbleNav.stories.ts
@@ -1,6 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import BubbleNav from '../app/components/BubbleNav';
 
+const menuItems = [
+    {name: "Item1"},
+    {name: "LongItem2"},
+    {name: "Item3"},
+    {name: "Super Long Item3", href: "/super-long-item3"},
+]
+const active = 2;
+
 const meta = {
     title: 'Minimalist/Nav',
     component: BubbleNav,
@@ -10,6 +18,10 @@ const meta = {
     tags: ['autodocs'],
     argTypes: {
         // backgroundColor: { control: 'color' },
+        active: { control: {
+            type: 'number',
+            min: 0, max: menuItems.length, step: 1
+            }},
     },
 } satisfies Meta<typeof BubbleNav>;
 
@@ -17,5 +29,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {},
+    args: {
+        menuItems,
+        active,
+    },
 };
