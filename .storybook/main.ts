b4218a039dfb84ab2469f4859381d785afa0b87a
DBryja <xileran@gmail.com>
2024-03-29 13:04:59 +0100

    Repair main.ts

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,7 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import path from "path";
 
 const config: StorybookConfig = {
-  stories: [
-      path.resolve(__dirname, "../src/**/*.mdx"),
-      path.resolve(__dirname, "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)")
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
