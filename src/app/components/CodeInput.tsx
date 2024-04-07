fb290c37f1f98f8d6fadb7d7be46521d29bff323
DBryja <xileran@gmail.com>
2024-04-07 12:57:48 +0200

    Update responsiveness, Limit to numbers, Form validation, MouseUp bugfix, Update paste

--- src/app/components/CodeInput.tsx
+++ src/app/components/CodeInput.tsx
@@ -4,7 +4,8 @@ import React, {useRef,useEffect} from "react";
 interface CodeInputProps {
     buildList: (number|string)[];
 }
-const regularExpression = /^[a-zA-Z0-9`~!@#$%^&*()-_=+[{\]}\\|;:'",<.>/? ]$/;
+// const regularExpression = /^[a-zA-Z0-9`~!@#$%^&*()-_=+[{\]}\\|;:'",<.>/? ]$/;
+const isNumber = (char : string) => /^\d$/.test(char);
 
 export default function CodeInput({buildList}: CodeInputProps) {
     const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
@@ -80,21 +81,35 @@ export default function CodeInput({buildList}: CodeInputProps) {
             case "ArrowUp":
                 event.preventDefault();
                 break;
+
+            // In case you want to allow the user to type in letters this is required to allow the paste event to work
             case "v":
                 //this fallthrough is intended to allow paste event
                 if (event.ctrlKey || event.metaKey) {
                     break;
                 }
             default:
-                if (regularExpression.test(event.key) && inputRefs.current[index]) {
+                if (isNumber(event.key) && inputRefs.current[index]) {
                     event.preventDefault();
                     //@ts-ignore
                     inputRefs.current[index].value = event.key;
                     if (index !== inputRefs.current?.length - 1) moveRight(index);
                 }
+                event.preventDefault();
                 break;
         }
     };
+
+    const getFirstEmptyInputIndex = (addon: (-1 | 0 | 1) = 0) => {
+        // If no input is currently focused, or it's not in the refs array, return the index of the first non-empty input
+        for (let i = 0; i < inputRefs.current.length; i++) {
+            if (inputRefs.current[i] && inputRefs.current[i]?.value === "") {
+                return i + addon;
+            }
+        }
+        // if no input is filled, return the index of the last input
+        return inputRefs.current?.length - 1;
+    }
     // Get the index of the currently focused input
     // If getCurrentInputIndex is called upon pressing an arrow key you should use addon +1 or -1 to get the correct index
     const getCurrentInputIndex = (addon: (-1 | 0 | 1) = 0) => {
@@ -102,14 +117,7 @@ export default function CodeInput({buildList}: CodeInputProps) {
         if (focusedInput && inputRefs.current.includes(focusedInput)) {
             return inputRefs.current.findIndex(input => input === focusedInput);
         } else {
-            // If no input is currently focused, or it's not in the refs array, return the index of the first non-empty input
-            for (let i = 0; i < inputRefs.current.length; i++) {
-                if (inputRefs.current[i] && inputRefs.current[i]?.value === "") {
-                    return i + addon;
-                }
-            }
-            // if no input is filled, return the index of the last input
-            return inputRefs.current?.length - 1;
+            return getFirstEmptyInputIndex(addon);
         }
     }
     // Fill inputs on paste
@@ -121,8 +129,16 @@ export default function CodeInput({buildList}: CodeInputProps) {
         const pasteArray = paste.split("");
         for (let i = 0; i < pasteArray.length; i++) {
             //@ts-ignore
-            if (inputRefs.current[i]) inputRefs.current[i].value = pasteArray[i];
+            if (inputRefs.current[i]) inputRefs.current[i].value = isNumber(pasteArray[i]) ? pasteArray[i] : "";
         }
+        // set focus on the first empty input in case there was symbol different from a number in the pasted data
+        inputRefs.current[getFirstEmptyInputIndex()]?.focus();
+    }
+
+    function handleInputMouseUp(e: React.MouseEvent<HTMLInputElement, MouseEvent>) {
+        const target = e.target as HTMLInputElement;
+        const len = target.value.length;
+        target.setSelectionRange(len, len);
     }
 
     function concatResult() {
@@ -143,32 +159,46 @@ export default function CodeInput({buildList}: CodeInputProps) {
         return resCode;
     }
 
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        alert(concatResult());
+    }
+
     let indexOfInput = 0;
     return (
         <section className={"flex flex-col gap-y-8 justify-center items-center"}>
-        <form className={"flex flex-row gap-x-4 items-center justify-center"}>
-            {buildList.map((symbol, index) => {
-                if (typeof symbol === "number") {
-                    let inputsArray: React.ReactElement[] = [];
-                    for (let i = 0; i < symbol; i++)
-                        {
+            <form className={"flex flex-row gap-x-4 items-center justify-center flex-wrap"} onSubmit={handleSubmit}>
+                {buildList.map((symbol, index) => {
+                    if (typeof symbol === "number") {
+                        let inputsArray: React.ReactElement[] = [];
+                        for (let i = 0; i < symbol; i++) {
                             inputsArray.push(
                                 <input key={indexOfInput}
                                        type={"text"}
-                                       className={"text-white w-8 h-12 bg-[#292c2e] border-radius-2 border border-white text-center focus:outline-dashed focus:outline-2 focus:outline-offset-2 rounded-lg transition-all"}
+                                       className={"text-white w-8 h-12 bg-[#292c2e] border-radius-2 border border-white text-center focus:outline-dashed focus:outline-2 focus:outline-offset-2 rounded-lg transition-all mb-2"}
                                        maxLength={1}
-                                       ref={el => {inputRefs.current.push(el)}}
+                                       ref={el => {
+                                           inputRefs.current.push(el)
+                                       }}
                                        onKeyDown={handleKeyDown(indexOfInput)}
                                        onChange={handleInputChange(indexOfInput)}
+                                       onMouseUp={handleInputMouseUp}
                                        required/>);
                             indexOfInput++;
                         }
-                    return inputsArray.map((input) => input);
-                } else return <span key={"span_"+index}>{symbol.toString()}</span>
-            })}
-        </form>
-        <button className={"px-12 py-2 border rounded-[50px] bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500"} onClick={()=>alert(concatResult())}>Submit</button>
-        <button className={"relative after:block after:absolute after:w-1/2 after:top-[120%] after:left-1/2 after:-translate-x-1/2 after:h-0.5 after:bg-white after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:ease-in-out"}><a href={"https://github.com/DBryja/Storybook-Plaground/blob/master/src/app/components/CodeInput.tsx"}>Code by Dawid Bryja</a></button>
+                        return inputsArray.map((input) => input);
+                    } else return <span key={"span_" + index}
+                                        className={"max-lg:grow-1 max-lg:w-full max-lg:text-center"}>{symbol.toString()}</span>
+                })}
+                <div className={"basis-full h-8"}/>
+                <input
+                    className={"px-12 py-2 border rounded-[50px] bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 cursor-pointer"}
+                    value={"Submit"}
+                    type={"submit"}>
+                </input>
+            </form>
+            <button
+                className={"relative after:block after:absolute after:w-1/2 after:top-[120%] after:left-1/2 after:-translate-x-1/2 after:h-0.5 after:bg-white after:scale-x-0 hover:after:scale-x-100 after:transition-transform after:ease-in-out"}><a href={"https://github.com/DBryja/Storybook-Plaground/blob/master/src/app/components/CodeInput.tsx"}>Code by Dawid Bryja</a></button>
     </section>
     );
 }
